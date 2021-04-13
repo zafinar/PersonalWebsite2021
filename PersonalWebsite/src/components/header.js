a7fc5de8369f43c90be21f2982695f50b32e4f53
zafinar <zafinar@gmail.com>
2021-04-13 18:18:35 -0400

    stripping starter

--- PersonalWebsite/src/components/header.js
+++ PersonalWebsite/src/components/header.js
@@ -4,17 +4,10 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+     
     >
       <h1 style={{ margin: 0 }}>
         <Link
